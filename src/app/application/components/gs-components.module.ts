2ae8c9c4b532cab028ef08c99d6fee8a5a0debf9
Gabriel Souza <gabrielsouzadevweb@gmail.com>
2023-04-04 21:42:15 -0300

    feat:resto

--- src/app/application/components/gs-components.module.ts
+++ src/app/application/components/gs-components.module.ts
@@ -1,15 +1,22 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
+import { NzButtonModule } from 'ng-zorro-antd/button';
 import { NzCheckboxModule } from 'ng-zorro-antd/checkbox';
 import { NzDividerModule } from 'ng-zorro-antd/divider';
 import { NzFormModule } from 'ng-zorro-antd/form';
+import { NzIconModule } from 'ng-zorro-antd/icon';
 import { NzRadioModule } from 'ng-zorro-antd/radio';
 import { NzTableModule } from 'ng-zorro-antd/table';
+import { NzToolTipModule } from 'ng-zorro-antd/tooltip';
+import { GsFabComponent } from './gs-fab/gs-fab.component';
 import { GsTableComponent } from './gs-table/gs-table.component';
 @NgModule({
-  declarations: [GsTableComponent],
+  declarations: [GsTableComponent, GsFabComponent],
   imports: [
+    NzToolTipModule,
+    NzButtonModule,
+    NzIconModule,
     CommonModule,
     ReactiveFormsModule,
     NzFormModule,
